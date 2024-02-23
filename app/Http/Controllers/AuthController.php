68e540beb0a243cb4d09d3bdd0f17782a0159537
Emmanuel <49695282+septierg@users.noreply.github.com>
2024-02-22 20:37:50 -0500

    keep working man

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        //dd($request->all());
+        //validation
+        $validated = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:6', 'confirmed'],
+
+        ]);
+
+        dd($validated);
+        //create user and token
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+            'api_token' => Str::random(60),
+        ]);
+
+
+        //assign role
+        $employee_role = Role::where('name', 'employee')->first();
+        $employee_role->user()->save($user);
+
+
+        //return response
+        $response = [
+            'user' => $user,
+            'access_token' => $user->api_token
+        ];
+
+        return response()->json($response, 201);
+
+
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,29 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:api')->group(function (Request $request) {
+    return 'api';
+    return $request->user();
+});
+Route::middleware('auth:api')->get('/auth', function () {
+    return 'Hello World';
+});
+Route::post('/register', function () {
+    return 'Hello World';
+});
+
+Route::post('/register', function (Request $request) {
+    return $request->all();
+    //return 'register in api';
+});
+Route::post('/register', [AuthController::class, 'register']);*/
+Route::post('/register', 'AuthController@register');
+/*Route::controller(AuthController::class)->group(function(){
+    Route::post('/register', 'register');
+});*/
